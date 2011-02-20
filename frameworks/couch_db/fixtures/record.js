3361d51f29f46a5efc12139bec7eb4a40c23e396
Alexey Shevchenko <otetz@me.com>
2011-02-20 03:42:15 +0300

    Create basic framework structure.

---
+++ frameworks/couch_db/fixtures/record.js
@@ -0,0 +1,14 @@
+// ==========================================================================
+// Project:   CouchDB.Record Fixtures
+// Copyright: ©2011 Alexey Shevchenko
+// ==========================================================================
+/*globals CouchDB */
+
+sc_require('models/record');
+
+CouchDB.Record.FIXTURES = [
+
+   { _id: 1 },
+   { _id: 2 }
+
+];
---
+++ frameworks/couch_db/models/record.js
@@ -0,0 +1,17 @@
+// ==========================================================================
+// Project:   CouchDB.Record
+// Copyright: ©2011 Alexey Shevchenko
+// ==========================================================================
+/*globals CouchDB */
+
+/** @class
+
+  (Document your Model here)
+
+  @extends SC.Record
+  @version 0.1
+*/
+CouchDB.Record = SC.Record.extend(
+/** @scope CouchDB.Record.prototype */ {
+
+}) ;
